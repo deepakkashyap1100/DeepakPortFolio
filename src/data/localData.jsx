3c22abe12dd7e9b7fb2bc16660d99e959628801f
voucherinfinity <voucherinfinity9@gmail.com>
2026-04-17 14:39:19 +0530

    update projects

--- src/data/localData.jsx
+++ src/data/localData.jsx
@@ -28,27 +28,9 @@ export const projectData = [
     ],
   },
 
+
   {
     id: 2,
-    projectImage: aeturn,
-    projectName: "AETURN",
-    desc: 'AETURN is a technology-driven company that delivers healthcare-focused digital solutions — from telemedicine and EMR/PHR systems to medical-tourism support, road-safety and fleet-tracking services. They aim to “bring solutions which impact lives,” by combining modern tools like AI/ML with a globally minded, collaborative approach.',
-    usedTech: ['ReactJs',
-      'Tailwind CSS',
-
-
-    ],
-    features: ['AI- & AR-powered sizing & body-measurement software',
-      'Virtual try-on / virtual-trial rooms',
-      "Digital fashion & metaverse-ready garments",
-      "Omnichannel & scalable retail solutions for brands"
-
-    ],
-    liveURL: 'https://www.aeturn.com/',
-    // sourceCode: 'https://github.com/deepakgitofficial/Form_in_react_01/tree/react_form_V_1.02',
-  },
-  {
-    id: 3,
     projectImage: aaiena,
     projectName: "Aaiena",
     desc: 'Aaiena is an AI-powered fashion-retail platform that helps brands deliver personalized, immersive shopping experiences through technologies like augmented reality (AR), mixed reality, and 3D virtual-fitting solutions. It aims to solve common apparel - shopping problems — such as size / fit mismatches, high return rates, and poor online shopping confidence — by enabling realistic virtual try-ons, fit predictions, and data - driven product recommendations.',
@@ -65,7 +47,7 @@ export const projectData = [
     ],
   },
   {
-    id: 4,
+    id: 3,
     projectImage: betterlyf,
     projectName: "betterlyf",
     desc: 'BetterLYF is an online counseling and mental-wellness platform offering professional support for stress, anxiety, depression, relationships, self-esteem, and personal challenges. Users can connect with certified counselors through chat, phone, or video sessions for convenient and confidential therapy. The platform also provides self-help programs and tools to improve emotional resilience, build confidence, and manage daily stress. With a focus on accessibility and privacy, BetterLYF aims to make mental-health support more affordable and approachable for individuals and organizations through its wellness and employee assistance programs.',
